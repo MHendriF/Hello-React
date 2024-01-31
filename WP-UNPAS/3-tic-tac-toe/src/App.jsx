9ca729e6ff9bf61c6c00e96913aa7fa6f56bc424
MHendriF <mhendrif@akupintar.id>
2024-01-31 14:39:09 +0700

    setup player

--- WP-UNPAS/3-tic-tac-toe/src/App.jsx
+++ WP-UNPAS/3-tic-tac-toe/src/App.jsx
@@ -11,11 +11,17 @@ function Square({ value, onSquareClick }) {
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
   
   function handleClick(i) {
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
