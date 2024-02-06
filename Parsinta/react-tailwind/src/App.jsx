8f0e5faf76804831f9bbf26474273ec1b3fe1b25
MHendriF <hendrifebriansyah28@gmail.com>
2024-02-06 10:45:34 +0700

    use tailwind style

--- Parsinta/react-tailwind/src/App.jsx
+++ Parsinta/react-tailwind/src/App.jsx
@@ -1,18 +1,17 @@
-import { IconBrandTwitter } from '@tabler/icons-react';
+import { IconBrandFacebook, IconBrandTwitter } from '@tabler/icons-react';
 
 export default function App() {
     return (
         <div className='bg-slate-900 grid place-content-center min-h-screen'>
             <div className='flex gap-x-2'>
-                <Button text='Register'></Button>
-                <Button
-                    text={
-                        <>
-                            <IconBrandTwitter />
-                            Login
-                        </>
-                    }
-                ></Button>
+                <Button>
+                    <IconBrandFacebook />
+                    Register
+                </Button>
+                <Button>
+                    <IconBrandTwitter />
+                    Login
+                </Button>
             </div>
         </div>
     );
@@ -20,7 +19,11 @@ export default function App() {
 
 function Button(props) {
     return (
-        <button className='flex items-center bg-blue-600 text-white px-4 py-2 rounded'>
+        <button
+            className={
+                '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 bg-blue-600 text-white px-4 py-2 rounded'
+            }
+        >
             {props.text || props.children}
         </button>
     );
