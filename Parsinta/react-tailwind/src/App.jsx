84022c69a2be1634091bb3cbb0fee302c7b8cf54
MHendriF <hendrifebriansyah28@gmail.com>
2024-02-06 10:41:24 +0700

    import icons tabler

--- Parsinta/react-tailwind/src/App.jsx
+++ Parsinta/react-tailwind/src/App.jsx
@@ -1,7 +1,27 @@
+import { IconBrandTwitter } from '@tabler/icons-react';
+
 export default function App() {
     return (
-        <div className='bg-blue 600 grid place-content-center'>
-            <h1 className={'text-h1'}>Learn react</h1>
+        <div className='bg-slate-900 grid place-content-center min-h-screen'>
+            <div className='flex gap-x-2'>
+                <Button text='Register'></Button>
+                <Button
+                    text={
+                        <>
+                            <IconBrandTwitter />
+                            Login
+                        </>
+                    }
+                ></Button>
+            </div>
         </div>
+    );
+}
+
+function Button(props) {
+    return (
+        <button className='flex items-center bg-blue-600 text-white px-4 py-2 rounded'>
+            {props.text || props.children}
+        </button>
     );
 }
