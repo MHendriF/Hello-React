060ef09f3cc64a845893d8a5dfa73c99da365b9a
MHendriF <hendrifebriansyah28@gmail.com>
2024-02-06 11:06:03 +0700

    react component

--- Parsinta/react-tailwind/src/App.jsx
+++ Parsinta/react-tailwind/src/App.jsx
@@ -25,17 +25,30 @@ export default function App() {
                     <IconBrandTwitter />
                     Logout
                 </Button>
+                <Button
+                    className='bg-green-500'
+                    onClick={() => console.log('Register')}
+                >
+                    <IconBrandTwitter />
+                    Exit
+                </Button>
             </div>
         </div>
     );
 }
 
 function Button(props) {
-    const { className = 'bg-blue-600', children, text, type } = props;
+    const {
+        className = 'bg-blue-600',
+        children,
+        text,
+        type = 'submit',
+    } = props;
 
     return (
         <button
             {...props}
+            type={type}
             className={clsx(
                 className,
                 '[&>svg]:w-5 [&>svg]:h-5 [&>svg]:stroke-1 flex items-center gap-x-2 text-white px-4 py-2 rounded'
