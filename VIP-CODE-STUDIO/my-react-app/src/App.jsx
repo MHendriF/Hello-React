b672ff4b083e15382fa474e8f2f11b8f8b2d8c3f
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-13 09:39:07 +0700

    Component & Props

--- VIP-CODE-STUDIO/my-react-app/src/App.jsx
+++ VIP-CODE-STUDIO/my-react-app/src/App.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 
-class Button extends React.Component {
-    render() {
-        return (
-            <button className='h-10 px-6 font-semibold rounded-md bg-slate-700 text-white' type='submit'>
-                Buy now
-            </button>
-        );
-    }
-}
-
-function ButtonBlack() {
+const Button = (props) => {
+    const { children = '...', variant = 'bg-black' } = props;
     return (
-        <button className='h-10 px-6 font-semibold rounded-md bg-black text-white' type='submit'>
-            Buy now
-        </button>
-    );
-}
-
-const ButtonRed = () => {
-    return (
-        <button className='h-10 px-6 font-semibold rounded-md bg-red-700 text-white' type='submit'>
-            Buy now
+        <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type='submit'>
+            {children}
         </button>
     );
 };
@@ -30,11 +13,9 @@ function App() {
     return (
         <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
             <div className='flex gap-x-3'>
-                <Button></Button>
-                <Button></Button>
-                <Button></Button>
-                <ButtonBlack></ButtonBlack>
-                <ButtonRed></ButtonRed>
+                <Button variant='bg-red-700'>Login</Button>
+                <Button variant='bg-slate-700'>Logout</Button>
+                <Button variant='bg-green-700'></Button>
             </div>
         </div>
     );
