a72a3a69c7bbaf83f7f90349b5fe02a389754a44
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-14 09:11:42 +0700

    Rendering Lists

--- VIP-CODE-STUDIO/my-react-app/src/components/Fragments/CardProduct.jsx
+++ VIP-CODE-STUDIO/my-react-app/src/components/Fragments/CardProduct.jsx
@@ -2,7 +2,7 @@ import Button from '../Elements/Buttons';
 
 const CardProduct = (props) => {
     const { children } = props;
-    return <div className='w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2'>{children}</div>;
+    return <div className='w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-2 flex flex-col justify-between'>{children}</div>;
 };
 
 const Header = (props) => {
@@ -18,7 +18,7 @@ const Header = (props) => {
 const Body = (props) => {
     const { children, title } = props;
     return (
-        <div className='px-5 pb-5'>
+        <div className='px-5 pb-5 h-full'>
             <a href='#'>
                 <h5 className='text-xl font-semibold tracking-tight text-white'>{title}</h5>
                 <p className='text-m text-white'>{children}</p>
--- VIP-CODE-STUDIO/my-react-app/src/pages/product.jsx
+++ VIP-CODE-STUDIO/my-react-app/src/pages/product.jsx
@@ -1,25 +1,33 @@
-import Button from '../components/Elements/Buttons';
 import CardProduct from '../components/Fragments/CardProduct';
+
+const products = [
+    {
+        id: 1,
+        name: 'Sepatu baru',
+        price: 'Rp 1.000.000',
+        image: '/images/shoes-1.jpg',
+        description:
+            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui a unde consequatur magnam odio ratione sunt impedit et? Dolore rerum amet iste praesentium magni tempora eos quidem pariatur culpa neque?',
+    },
+    {
+        id: 2,
+        name: 'Sepatu lama',
+        price: 'Rp 500.000',
+        image: '/images/shoes-1.jpg',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+];
 
 const ProductsPage = () => {
     return (
         <div className='flex justify-center'>
-            <CardProduct>
-                <CardProduct.Header image='/images/shoes-1.jpg' />
-                <CardProduct.Body title='Sepatu Baru'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui a unde consequatur magnam odio ratione sunt impedit et? Dolore rerum
-                    amet iste praesentium magni tempora eos quidem pariatur culpa neque?
-                </CardProduct.Body>
-                <CardProduct.Footer price='Rp 1.000.000' />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image='/images/shoes-1.jpg' />
-                <CardProduct.Body title='Sepatu Baru'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui a unde consequatur magnam odio ratione sunt impedit et? Dolore rerum
-                    amet iste praesentium magni tempora eos quidem pariatur culpa neque?
-                </CardProduct.Body>
-                <CardProduct.Footer price='Rp 1.000.000' />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct key={product.id}>
+                    <CardProduct.Header image={product.image}></CardProduct.Header>
+                    <CardProduct.Body title={product.name}>{product.description}</CardProduct.Body>
+                    <CardProduct.Footer price={product.price}></CardProduct.Footer>
+                </CardProduct>
+            ))}
         </div>
     );
 };
