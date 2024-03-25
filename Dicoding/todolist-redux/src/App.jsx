8fdaae16ffc9b462da2e8a6e6627ac2709330104
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-25 12:04:01 +0700

    update code

---
+++ Dicoding/todolist-redux/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+import GoalsList from './components/GoalsList';
+
+function App() {
+    return (
+        <>
+            <TodoList />
+            <GoalsList />
+        </>
+    );
+}
+
+export default App;
--- Dicoding/todolist-redux/src/states/index.js
+++ Dicoding/todolist-redux/src/states/index.js
@@ -1,4 +1,4 @@
-import { applyMiddleware, createStore } from 'redux';
+import { applyMiddleware, createStore } from '@reduxjs/toolkit';
 import { todoDeletionCheck } from './middlewares';
 import rootReducer from './rootReducer';
 
