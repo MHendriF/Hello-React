9bcd474053e4b312d1892a916c5aa36d11eb7001
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-26 10:24:12 +0700

    Membuat Action dan Reducer untuk State talkDetail

--- Dicoding/open-space-app-starter/src/states/talkDetail/action.js
+++ Dicoding/open-space-app-starter/src/states/talkDetail/action.js
@@ -1,3 +1,62 @@
 /**
  * @TODO: Define all the actions (creator) for the talkDetail state
  */
+import ActionType from '../../utils/actionType';
+import api from '../../utils/api';
+
+function receiveTalkDetailActionCreator(talkDetail) {
+    return {
+        type: ActionType.RECEIVE_TALK_DETAIL,
+        payload: {
+            talkDetail,
+        },
+    };
+}
+
+function clearTalkDetailActionCreator() {
+    return {
+        type: ActionType.CLEAR_TALK_DETAIL,
+    };
+}
+
+function toggleLikeTalkDetailActionCreator(userId) {
+    return {
+        type: ActionType.TOGGLE_LIKE_TALK_DETAIL,
+        payload: {
+            userId,
+        },
+    };
+}
+
+function asyncReceiveTalkDetail(talkId) {
+    return async (dispatch) => {
+        dispatch(clearTalkDetailActionCreator());
+        try {
+            const talkDetail = await api.getTalkDetail(talkId);
+            dispatch(receiveTalkDetailActionCreator(talkDetail));
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+}
+
+function asyncToogleLikeTalkDetail() {
+    return async (dispatch, getState) => {
+        const { authUser, talkDetail } = getState();
+        dispatch(toggleLikeTalkDetailActionCreator(authUser.id));
+
+        try {
+            await api.toggleLikeTalk(talkDetail.id);
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+}
+
+export {
+    receiveTalkDetailActionCreator,
+    clearTalkDetailActionCreator,
+    toggleLikeTalkDetailActionCreator,
+    asyncReceiveTalkDetail,
+    asyncToogleLikeTalkDetail,
+};
