cbff3fefe009ccd2132045a9b6c094762aa8ddf3
MHendriF <hendrifebriansyah28@gmail.com>
2024-03-26 10:26:23 +0700

    Membuat Redux Store

--- Dicoding/open-space-app-starter/src/states/index.js
+++ Dicoding/open-space-app-starter/src/states/index.js
@@ -1,3 +1,21 @@
 /**
  * @TODO: Create Redux store
  */
+import { configureStore } from '@reduxjs/toolkit';
+import authUserReducer from './authUser/reducer';
+import isPreloadReducer from './isPreload/reducer';
+import talkDetailReducer from './talkDetail/reducer';
+import talksReducer from './talks/reducer';
+import usersReducer from './users/reducer';
+
+const store = configureStore({
+    reducer: {
+        authUser: authUserReducer,
+        isPreload: isPreloadReducer,
+        users: usersReducer,
+        talks: talksReducer,
+        talkDetail: talkDetailReducer,
+    },
+});
+
+export default store;
