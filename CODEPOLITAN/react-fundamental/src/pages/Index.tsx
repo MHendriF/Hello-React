d838b996aa052ab95a78544b66df3839cb06af8e
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-17 14:13:29 +0700

    basic usestate

--- CODEPOLITAN/react-fundamental/src/pages/Index.tsx
+++ CODEPOLITAN/react-fundamental/src/pages/Index.tsx
@@ -1,12 +1,23 @@
 import { posts } from "../posts";
 import Article from "../components/Article";
+import { useState } from "react";
 
 export default function HomePage() {
+  const [search, setSearch] = useState<string>("");
+  const onChangeSearch = (e: any) => {
+    console.log(e.target.value);
+    setSearch(e.target.value);
+  };
+
   return (
     <div>
       <h1>Simple Blog</h1>
-      {posts.map((post) => (
-        <Article key={post.id} title={post.title} tags={post.tags} date={post.date} />
+      <div>
+        Cari Article: <input onChange={onChangeSearch} type="text" />
+      </div>
+      <small>Ditemukan 0 data dengan pencarian kata {search}</small>
+      {posts.map(({ title, tags, date }, index) => (
+        <Article key={index} {...{ title, tags, date }} />
       ))}
     </div>
   );
