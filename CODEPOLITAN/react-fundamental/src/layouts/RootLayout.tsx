7616b52f0cc0f8e8bb0a975334e138f85d9385ff
MHendriF <hendrifebriansyah28@gmail.com>
2024-07-18 09:19:22 +0700

    layouting dgn children routes

---
+++ CODEPOLITAN/react-fundamental/src/layouts/RootLayout.tsx
@@ -0,0 +1,10 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <>
+      <Link to="/">Home</Link> |<Link to="/about">About</Link> |<Link to="/contact">Contact</Link> |
+      <Outlet />
+    </>
+  );
+}
--- CODEPOLITAN/react-fundamental/src/routers/index.tsx
+++ CODEPOLITAN/react-fundamental/src/routers/index.tsx
@@ -1,17 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/Index";
+import AboutPage from "../pages/About";
+import ContactPage from "../pages/Contact";
+import RootLayout from "../layouts/RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/about",
-    element: <div>About</div>,
-  },
-  {
-    path: "/contact",
-    element: <div>Contact</div>,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/about",
+        element: <AboutPage />,
+      },
+      {
+        path: "/contact",
+        element: <ContactPage />,
+      },
+    ],
   },
 ]);
